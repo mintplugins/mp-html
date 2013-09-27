774e1c13fbb2bc3645fdd057f8cfcf2a4a1c30e4
Phil Johnston <johnstonphilip@gmail.com>
2013-09-27 15:51:40 -0400

    1.0.0.3
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/updater/mp-html-update.php
+++ includes/updater/mp-html-update.php
@@ -1,13 +1,29 @@
 <?php
 /**
- * Check for updates for this Plugin
+ * This file contains the function keeps the MP HTML plugin up to date.
  *
+ * @since 1.0.0
+ *
+ * @package    MP HTML
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2013, Move Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
+ */
+ 
+/**
+ * Check for updates for the MP HTML Plugin by creating a new instance of the MP_CORE_Plugin_Updater class.
+ *
+ * @access   public
+ * @since    1.0.0
+ * @return   void
  */
  if (!function_exists('mp_html_update')){
 	function mp_html_update() {
 		$args = array(
-			'software_name' => 'MP HTML In Post', //<- The exact name of this Plugin. Make sure it matches the title in your mp_repo, edd, and the WP.org repo
-			'software_api_url' => 'http://repo.moveplugins.com',//The URL where EDD and mp_repo are installed and checked
+			'software_name' => 'MP HTML', //<- The exact name of this Plugin. Make sure it matches the title in your mp_repo, edd, and the WP.org repo
+			'software_api_url' => 'http://moveplugins.com',//The URL where EDD and mp_repo are installed and checked
 			'software_filename' => 'mp-html.php',
 			'software_licensed' => false, //<-Boolean
 		);
